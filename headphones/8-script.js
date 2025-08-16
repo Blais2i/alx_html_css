b7159dc17c0f35865a6d69f4ad30a19d0d015b90
Blaise Iradukunda <blaisealkado@gmail.com>
2025-08-16 14:49:07 -0700

    Add all changes made

---
+++ headphones/8-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobileMenu.classList.toggle('active');
+    });
+});
